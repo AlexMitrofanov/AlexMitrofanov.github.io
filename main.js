c8f95a0a326bd4491491c44c0a2121a427250d85
AlexMitrofanov <jeep_95@mail.ru>
2016-04-05 22:07:22 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,15 @@
-//JS samples
-function showHref() {
-    alert(location.href);
-}
+<html>
+    <head>
+        <title>Документ</title>
+    </head>
+    <body>
+        <div id="dataKeeper">Data</div>
+        <ul>
+            <li style="background-color:red">Осторожно</li>
+            <li class="info">Информация</li>
+        </ul>
+        <div id="footer">Made in Russia &copy;</div>
 
-//BOM samples
-function showAgent() {
-    alert(navigator.userAgent);
-}
+    </body>
 
-//DOM samples
-function makeBGRed() {
-    if (document.body.style.background.valueOf() === new String("red").valueOf()) {
-        document.body.style.background = "";
-    } else {
-        document.body.style.background = "red";
-    }
-}
-
-//jQuery
-
-$(document).ready(function() {
-    $("#fadeinout").click(function() {
-        $(this).fadeOut(1000).fadeIn(1000);
-    });
-});
+</html>
