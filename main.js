6651991b0a6515a4f5f6f968680e4917f97c5368
AlexMitrofanov <jeep_95@mail.ru>
2016-04-05 23:17:05 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
 <html>
-    <head>
-        <title>Документ</title>
-    </head>
-    <body>
-        <div id="dataKeeper">Data</div>
-        <ul>
-            <li style="background-color:red">Осторожно</li>
-            <li class="info">Информация</li>
-        </ul>
-        <div id="footer">Made in Russia &copy;</div>
+<body>
 
-    </body>
+Имя домена сервера, который загрузил этот документ: 
+<script type="text/javascript">
+document.write(document.domain);
+</script>
 
+</body>
 </html>
